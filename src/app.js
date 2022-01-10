1e7be3605bf14e1d3a89316d12b71ccf19f2f197
ajedwin15 <ajedwin7@gmail.com>
2022-01-10 12:54:04 -0500

    config proyect S3

---
+++ src/app.js
@@ -0,0 +1,5 @@
+const Server = require('./server');
+
+const app = new Server();
+
+app.listen();
---
+++ src/controllers/upload.controller.js
@@ -0,0 +1,8 @@
+const upload = (req, res) => {
+    console.log(req);
+    res.send('Upload')
+}
+
+module.exports = {
+    upload
+}
